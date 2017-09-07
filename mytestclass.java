59f6802f699b560046de3d73c9a1dbf986f378d5
Mat Sawyer <13465586+matsawyer@users.noreply.github.com>
2017-09-06 18:39:42 -0700

    barrel rolls
    needed

--- mytestclass.java
+++ mytestclass.java
@@ -2,29 +2,26 @@ import com.sun.xml.internal.bind.XmlAccessorFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.Keys;
 
 /**
- * Created by mattsawyer on 11/14/16.
+ * Created by M@t
  */
 //#define firefoxLocation
-public class mytestclass {
+public class mySimpleSeleniumExample {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver","/Users/mattsawyer/Downloads/geckodriver");
+        System.setProperty("webdriver.gecko.driver","~/Downloads/geckodriver");
         //New Firefox window
         WebDriver driver = new FirefoxDriver();
-
         //maximize window
         driver.manage().window().maximize();
-
         //opens specified url in browser
         driver.get("https://www.google.com");
-
-        driver.findElement(By.name("q")).sendKeys("test writing inside of the search bar");
-
+        //barrel roll necessary 
+        driver.findElement(By.name("q")).sendKeys("do a barrel roll" + Keys.RETURN);
         String CurrentURL = driver.getCurrentUrl();
         System.out.println("My Current URL is : " + CurrentURL);
-        driver.close();
-
+        driver.close(); //this kills the window
     }
 }
